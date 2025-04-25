fbb182ab48581b21f2d6fb353adb777457991379
DenerMilGrau <leitenegridener@gmail.com>
2025-04-25 11:11:05 -0300

    a

--- app.py
+++ app.py
@@ -17,28 +17,41 @@ app.secret_key = 'chave_secreta'
 
 @app.route('/')
 def index():
+    """
+        API para gerenciar uma biblioteca integrada a um banco de dados
+
+
+
+    """
     return redirect('/livros')
 @app.route('/livros', methods=['GET'])
 @app.route('/livros/<status>', methods=['GET'])
 def get_livros(status=None):
-    if status is None:
-        lista_livros_sql = select(Livro)
-    else:
-        if status in ['1', 1, 'True', True]:
-            status_emprestimo = True
-        elif status in ['0', 0, 'False', False]:
-            status_emprestimo = False
+
+
+
+    try:
+        if status is None:
+            lista_livros_sql = select(Livro)
         else:
-            return jsonify({'result': 'Error. Requisitado uma variável inesperada'}), 400
+            if status in ['1', 1, 'True', True]:
+                status_emprestimo = True
+            elif status in ['0', 0, 'False', False]:
+                status_emprestimo = False
+            else:
+                return jsonify({'result': 'Error. Requisitado uma variável inesperada'}), 400
 
-        lista_livros_sql = select(Livro).where(Livro.status_emprestado == status_emprestimo)
+            lista_livros_sql = select(Livro).where(Livro.status_emprestado == status_emprestimo)
 
-    lista_livros = db_session.execute(lista_livros_sql).scalars()
-    result = []
-    for livro in lista_livros:
-        result.append(livro.serialize_livro())
+        lista_livros = db_session.execute(lista_livros_sql).scalars()
+        result = []
+        for livro in lista_livros:
+            result.append(livro.serialize_livro())
 
-    return jsonify({'result': result})
+        return jsonify({'result': result})
+    except IntegrityError:
+        return jsonify({'result': 'Error. Integrity Error (faltam informações ou informações corretas) '}), 400
+
 
 @app.route('/usuarios', methods=['GET'])
 @app.route('/usuarios/<id_user>', methods=['GET'])
@@ -72,44 +85,57 @@ def get_usuarios(id_user=None):
 
 @app.route('/emprestimos', methods=['GET'])
 def get_emprestimos():
-    lista_emprestimos_sql = select(Emprestimo).join(
-        Livro, Emprestimo.ID_livro == Livro.id_livro).join(
-        Usuario, Emprestimo.ID == Usuario.id
-    )
-    lista_emprestimos = db_session.execute(lista_emprestimos_sql).scalars().all()
-    print(lista_emprestimos)
-    result = []
-    for emprestimo in lista_emprestimos:
-        result.append(emprestimo.serialize_emprestimo())
+    try:
+        lista_emprestimos_sql = select(Emprestimo).join(
+            Livro, Emprestimo.ID_livro == Livro.id_livro).join(
+            Usuario, Emprestimo.ID == Usuario.id
+        )
+        lista_emprestimos = db_session.execute(lista_emprestimos_sql).scalars().all()
+        print(lista_emprestimos)
+        result = []
+        for emprestimo in lista_emprestimos:
+            result.append(emprestimo.serialize_emprestimo())
 
-    return jsonify({'result': result})
+        return jsonify({'result': result})
+    except IntegrityError:
+        return jsonify({'result': 'Error. Integrity Error (faltam informações ou informações corretas) '}), 400
+
 
 @app.route('/usuarios', methods=['POST'])
 def novo_usuario():
-    dados_usuario = request.get_json()
-    nome = dados_usuario['nome']
-    cpf = request.form['cpf']
-    telefone = request.form['telefone']
-    cpf = str(cpf)
-    if not nome or not cpf or not telefone or len(cpf) != 11:
-        return jsonify({'result': 'Error. Integrity Error (faltam informações) '}), 400
-    else:
-        cpf_f = '{0}.{1}.{2}-{3}'.format(cpf[:3], cpf[3:6], cpf[6:9], cpf[9:])
-        post = Usuario(nome=nome, cpf=cpf_f, telefone=telefone)
-        post.save()
-        db_session.close()
-        return jsonify({'result': 'Usuario criado com sucesso!'}), 200
+    try:
+        dados_usuario = request.get_json()
+        nome = dados_usuario['nome']
+        cpf = dados_usuario['cpf']
+        telefone = dados_usuario['telefone']
+        cpf = str(cpf)
+        if not nome or not cpf or not telefone or len(cpf) != 11:
+            return jsonify({'result': 'Error. Integrity Error (faltam informações) '}), 400
+        else:
+            cpf_f = '{0}.{1}.{2}-{3}'.format(cpf[:3], cpf[3:6], cpf[6:9], cpf[9:])
+            usuario = select(Usuario).where(Usuario.cpf == cpf_f)
+            usuario = db_session.execute(usuario).scalars()
+            if usuario:
+                raise TypeError
+            else:
+                post = Usuario(nome=nome, cpf=cpf_f, telefone=telefone)
+                post.save()
+                db_session.close()
+                return jsonify({'result': 'Usuario criado com sucesso!'}), 200
+    except TypeError:
+        return jsonify({'result': 'Error. Integrity Error (faltam informações ou informações corretas) '}), 400
 
 @app.route('/emprestimos', methods=['POST'])
 def novo_emprestimo():
-    # dados = request.get_json()
-    data_emprestimo = request.form['data_emprestimo']
-    data_devolucao = request.form['data_devolucao']
-    usuario_id = request.form['id_usuario']
-    livro_id = request.form['id_livro']
+    json_dados_emprestimo = request.get_json()
+    data_emprestimo = json_dados_emprestimo['data_emprestimo']
+    usuario_id = json_dados_emprestimo['id_usuario']
+    livro_id = json_dados_emprestimo['id_livro']
 
+    tempo_emprestimo = json_dados_emprestimo['tempo_emprestimo']
+    tipo_tempo = json_dados_emprestimo['tipo_tempo']
     try:
-        if len(data_emprestimo) == 10 or len(data_devolucao) == 10:
+        if len(data_emprestimo) == 10:
             usuario_id = int(usuario_id)
             livro_id = int(livro_id)
             # data formato yyyy-mm-dd
@@ -117,35 +143,52 @@ def novo_emprestimo():
             mes_emprestimo = '{}'.format(data_emprestimo[5:7])
             dia_emprestimo = '{}'.format(data_emprestimo[8:])
 
-            ano_devolucao = '{}'.format(data_devolucao[:4])
-            mes_devolucao = '{}'.format(data_devolucao[5:7])
-            dia_devolucao = '{}'.format(data_devolucao[8:])
-
             obj_data_emprestimo = date(int(ano_emprestimo), int(mes_emprestimo), int(dia_emprestimo))
-            obj_data_devolucao = date(int(ano_devolucao), int(mes_devolucao), int(dia_devolucao))
 
-            delta = obj_data_devolucao - obj_data_emprestimo
-            print('delta:', delta, 'delta days:', delta.days)
+            tipos_validos = ['d', 'w', 'm', 'y']
+            if tipo_tempo in tipos_validos:
+                data_devolucao = None
+                if tipo_tempo == 'd':
+                    data_devolucao = data_emprestimo + relativedelta(days=tempo_emprestimo)
 
-            if not data_emprestimo or not data_devolucao or not usuario_id or not livro_id or delta.days < 0:
-                raise ValueError
-            else:
-                livro_emprestado = select(Livro).where(Livro.id_livro == livro_id)
-                livro_emprestado = db_session.execute(livro_emprestado).scalar()
-                if not livro_emprestado.status_emprestado:
-                    post = Emprestimo(data_devolucao=data_devolucao,
-                                      data_emprestimo=data_emprestimo,
-                                      ID = usuario_id,
-                                      ID_livro =livro_id
-                                      )
-                    post.save()
-                    db_session.close()
-                    livro_emprestado.status = True
-                    return jsonify({'result': 'Emprestimo criado com sucesso!'}), 200
+                elif tipo_tempo == 'w':
+                    data_devolucao = data_emprestimo + relativedelta(weeks=tempo_emprestimo)
+
+                elif tipo_tempo == 'm':
+                    data_devolucao = data_emprestimo + relativedelta(months=tempo_emprestimo)
+
+                elif tipo_tempo == 'y':
+                    data_devolucao = data_emprestimo + relativedelta(years=tempo_emprestimo)
+
+                # ano_devolucao = '{}'.format(data_devolucao[:4])
+                # mes_devolucao = '{}'.format(data_devolucao[5:7])
+                # dia_devolucao = '{}'.format(data_devolucao[8:])
+
+                # obj_data_devolucao = date(int(ano_devolucao), int(mes_devolucao), int(dia_devolucao))
+
+                delta = data_devolucao - obj_data_emprestimo
+                print('delta:', delta, 'delta days:', delta.days)
+
+                if not data_emprestimo or not data_devolucao or not usuario_id or not livro_id or delta.days < 0:
+                    raise ValueError
                 else:
-                    return jsonify(
-                        {'result': 'Error. Integrity Error (faltam informações ou informações corretas) '}), 400
-
+                    livro_emprestado = select(Livro).where(Livro.id_livro == livro_id)
+                    livro_emprestado = db_session.execute(livro_emprestado).scalar()
+                    if not livro_emprestado.status_emprestado:
+                        post = Emprestimo(data_devolucao=data_devolucao,
+                                          data_emprestimo=data_emprestimo,
+                                          ID = usuario_id,
+                                          ID_livro =livro_id
+                                          )
+                        post.save()
+                        db_session.close()
+                        livro_emprestado.status = True
+                        return jsonify({'result': 'Emprestimo criado com sucesso!'}), 200
+                    else:
+                        return jsonify(
+                            {'result': 'Error. Integrity Error (faltam informações ou informações corretas) '}), 400
+            else:
+                raise ValueError
         else:
             raise ValueError
     except ValueError:
@@ -155,10 +198,11 @@ def novo_emprestimo():
 def novo_livro():
 
     try:
-        titulo = request.form['titulo']
-        autor = request.form['autor']
-        descricao = request.form['descricao']
-        isbn = request.form['isbn']
+        json_dados_livro = request.get_json()
+        titulo = json_dados_livro['titulo']
+        autor = json_dados_livro['autor']
+        descricao = json_dados_livro['descricao']
+        isbn = json_dados_livro['isbn']
 
         if not titulo or not autor or not isbn:
             raise ValueError
@@ -182,6 +226,7 @@ def editar_usuarios(id_user):
         id_usuario = id_user
         usuario_sql = select(Usuario).where(Usuario.id == id_usuario)
         usuario = db_session.execute(usuario_sql).scalar()
+
         json_dados_usuario = request.get_json()
         telefone = None
         cpf = None
@@ -207,7 +252,13 @@ def editar_usuarios(id_user):
                 if not cpf == '':
                     if len(cpf) == 11:
                         cpf_f = '{0}.{1}.{2}-{3}'.format(cpf[:3], cpf[3:6], cpf[6:9], cpf[9:])
-                        usuario.cpf = cpf_f
+                        select_usuario = select(Usuario).where(Usuario.cpf == cpf_f)
+                        sql_usuario = db_session.execute(select_usuario).scalars()
+                        if sql_usuario:
+                            print('cpf igaullllll')
+                            raise ValueError
+                        else:
+                            usuario.cpf = cpf_f
                     else:
                         raise ValueError
                 else:
@@ -235,36 +286,94 @@ def editar_usuarios(id_user):
         return jsonify({'result': 'Error. Integrity Error (Erro de integridade) '}), 400
 
 
-@app.route('/livros/<id_livro>', methods=['POST'])
+@app.route('/livros/<id_livro>', methods=['PUT'])
 def editar_livros(id_livro):
-    id_livro = id_livro
-    livro_sql = select(Livro).where(Livro.id_livro == id_livro)
-    livro = db_session.execute(livro_sql).scalar()
+    try:
+        id_livro = id_livro
+        livro_sql = select(Livro).where(Livro.id_livro == id_livro)
+        livro = db_session.execute(livro_sql).scalar()
+        json_dados_livro = request.get_json()
+        # status_emprestado = request.form['autor']
+        isbn = None
+        descricao = None
+        autor = None
+        titulo = None
+        if 'isbn' in json_dados_livro or 'titulo' in json_dados_livro or 'autor' in json_dados_livro or 'descricao' in json_dados_livro:
+            if 'isbn' in json_dados_livro:
+                isbn = json_dados_livro['isbn']
+            if 'titulo' in json_dados_livro:
+                titulo = json_dados_livro['titulo']
+            if 'autor' in json_dados_livro:
+                autor = json_dados_livro['autor']
+            if 'descricao' in json_dados_livro:
+                descricao = json_dados_livro['descricao']
 
-    isbn = request.form['isbn']
-    titulo = request.form['titulo']
-    autor = request.form['autor']
-    # status_emprestado = request.form['autor']
-    descricao = request.form['descricao']
+            if titulo:
+                if titulo == '':
+                    raise TypeError
+                else:
+                    livro.titulo = titulo
 
-    if not isbn or not titulo or not autor:
-        return jsonify({'result': 'Error. Integrity Error (faltam informações ou informações corretas) '}), 400
-    else:
-        livro.isbn = isbn
-        livro.titulo = titulo
-        livro.autor = autor
-        livro.descricao = descricao
-        livro.save()
-        return jsonify({'result': 'Livro editado com sucesso!'}), 200
+            if autor:
+                if autor == '':
+                    raise TypeError
+                else:
+                    livro.autor = autor
 
-@app.route('/emprestimos/<id_emp>', methods=['POST'])
+            if descricao:
+                livro.descricao = descricao
+
+            if isbn:
+                if isbn == '':
+                    raise TypeError
+                else:
+                    int(isbn)
+                    livro.isbn = isbn
+
+            livro.save()
+            return jsonify({'result': 'Livro editado com sucesso!'}), 200
+
+        else:
+            raise TypeError
+    except TypeError:
+        return jsonify({'result': 'Error. TypeError (faltam informações ou informações corretas) '}), 400
+    except ValueError:
+        return jsonify({'result': 'Error. ValueError (faltam informações ou informações corretas) '}), 400
+
+
+@app.route('/emprestimos/<id_emp>', methods=['PUT'])
 def editar_emprestimos(id_emp):
-    id_emprestimo = id_emp
-    emprestimo_sql = select(Emprestimo, Usuario, Livro).join(
-        Usuario, Emprestimo.ID == Usuario.id).join(
-        Livro, Emprestimo.ID_livro == Livro.id_livro).where(
-        Emprestimo.id_emprestimo == id_emprestimo)
-    emprestimo = db_session.execute(emprestimo_sql).scalar()
+    try:
+        id_emprestimo = id_emp
+        emprestimo_sql = select(Emprestimo, Usuario, Livro).join(
+            Usuario, Emprestimo.ID == Usuario.id).join(
+            Livro, Emprestimo.ID_livro == Livro.id_livro).where(
+            Emprestimo.id_emprestimo == id_emprestimo)
+        emprestimo = db_session.execute(emprestimo_sql).scalar()
+
+        json_dados_emprestimo = request.get_json()
+
+        if 'status' in json_dados_emprestimo:
+            status = json_dados_emprestimo['status']
+            if not status == '':
+                if status in ['True', 1, '1']:
+                    status = True
+                elif status in ['False', 0, '0']:
+                    status = False
+                else:
+                    raise ValueError
+                emprestimo.status_finalizado = status
+                emprestimo.save()
+                return jsonify({'result': 'Emprestimo editado com sucesso!'}), 200
+
+            else:
+                raise ValueError
+        else:
+            raise TypeError
+
+    except TypeError:
+        return jsonify({'result': 'Error. Integrity Error (faltam informações ou informações corretas) '}), 400
+
 
 
 
